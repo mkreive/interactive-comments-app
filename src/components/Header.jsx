c5c7f62fe2efc1c237a93fa4499e3c303eafccce
mkreive <mkreive@gmail.com>
2022-07-01 10:30:56 +0300

    navigation done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,20 +4,27 @@ import '../index.scss';
 const Header = function () {
     return (
         <header className='header'>
-            <div className='header__logo'></div>
-            <img
-                className='header__logo-logo'
-                src='https://res.cloudinary.com/kreiva/image/upload/v1655102872/DevJobsApp/creative_cdv7ub.svg'
-                alt='logo'
-            />
-            <h2 className='header__logo-name'>talk~y</h2>
+            <div className='header__logo'>
+                <img
+                    className='header__logo-logo'
+                    src='https://res.cloudinary.com/kreiva/image/upload/v1655102872/DevJobsApp/creative_cdv7ub.svg'
+                    alt='logo'
+                />
+                <h2 className='header__logo-name'>talk~y</h2>
+            </div>
             <nav className='header__nav'>
-                <ul>
-                    <li>About</li>
-                    <li>Topics</li>
-                    <li>Login</li>
+                <ul className='navigation'>
+                    <li className='navigation__item'>About</li>
+                    <li className='navigation__item'>Topics</li>
+                    <li className='navigation__item'>Contacts</li>
                 </ul>
             </nav>
+            <div className='signin'>
+                <input type='text' className='signin__input' placeholder='Username' />
+                <input type='text' className='signin__input' placeholder='email@email.com' />
+                <button className='btn'>Sign Up</button>
+                <button className='btn'>Log In</button>
+            </div>
         </header>
     );
 };
