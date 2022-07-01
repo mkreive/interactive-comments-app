ec10f8a9ddba36c4fcdccabf0a01cf8dab5d0f1a
mkreive <mkreive@gmail.com>
2022-07-01 16:35:59 +0300

    comments component in progress

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -17,11 +17,11 @@ const Comment = function () {
                             src='https://res.cloudinary.com/kreiva/image/upload/v1656680047/Interactive%20comments%20app/image-amyrobson_r3w5jz.png'
                             alt='user-photo'
                         />
-                        <h3 className='header-small'>ammyrobson</h3>
+                        <h3 className='header-small-colored'>ammyrobson</h3>
                         <span className='header-small-gray'>1 month ago</span>
-                        <div className='reply'>Reply</div>
+                        <div className='header-reply'>Reply</div>
                     </header>
-                    <div>
+                    <div className='text'>
                         Hi! I love dogs! All my life I had one or even few at home. But now I live with my partner and
                         he's alergic to dogs. He loves them too though. Maybe there is some breeds or ways to have a pet
                         without agregating allergy... Would like to hear your experiences?
