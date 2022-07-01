97a9c4fef0056270cfdbfc80bb9117b616d16da6
mkreive <mkreive@gmail.com>
2022-07-01 09:38:18 +0300

    main and header components created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,13 @@ import '../index.scss';
 const Header = function () {
     return (
         <header className='header'>
+            <div className='header__logo'></div>
             <img
-                className='header__logo'
+                className='header__logo-logo'
                 src='https://res.cloudinary.com/kreiva/image/upload/v1655102872/DevJobsApp/creative_cdv7ub.svg'
                 alt='logo'
             />
+            <h2 className='header__logo-name'>talk~y</h2>
             <nav className='header__nav'>
                 <ul>
                     <li>About</li>
