1616450678a75723ba6786b61c3fe3b4224f16df
mkreive <mkreive@gmail.com>
2022-07-04 10:10:14 +0300

    reply styling done

--- src/components/Reply.jsx
+++ src/components/Reply.jsx
@@ -4,7 +4,15 @@ import '../index.scss';
 const Reply = function () {
     return (
         <div className='card'>
-            <div className='reply'></div>
+            <div className='reply'>
+                <img
+                    className='images__photo--reply'
+                    src='https://res.cloudinary.com/kreiva/image/upload/v1656680047/Interactive%20comments%20app/image-juliusomo_lefxnc.webp'
+                    alt='user photo'
+                />
+                <textarea name='comment' id='123' rows='5' className='text--area'></textarea>
+                <button className='btn btn--blue'>Reply</button>
+            </div>
         </div>
     );
 };
