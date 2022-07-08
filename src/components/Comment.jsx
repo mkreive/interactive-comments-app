afae04a952b723575ef7c8983a08aab9225a4ad1
mkreive <mkreive@gmail.com>
2022-07-08 12:01:52 +0300

    replies styling done

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -26,7 +26,7 @@ const Comment = function (props) {
                     <article className='comment__main'>
                         <header className='comment__main__header'>
                             <img className='images__photo' src={`${comment.avatar}`} alt='user-photo' />
-                            <h3 className='header-small-colored'>{comment.username}</h3>
+                            <h3 className='header-small'>{comment.username}</h3>
                             <span className='header-small-gray'>{comment.createdAt}</span>
                             <div className='header-reply'>Reply</div>
                         </header>
