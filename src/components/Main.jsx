ec002d0f7e21f5dc50fbb3f524afa9ae81092f1b
mkreive <mkreive@gmail.com>
2022-08-09 09:34:43 +0300

    topic data created and rendering fixed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,26 @@
 import React, { useContext, useState } from 'react';
 import '../index.scss';
+import dataTopics from '../dataTopics.json';
 import Comment from './Comment';
 import UserContext from '../store/user-context';
 
 const Main = function () {
+    // states
     const [isActive, setIsActive] = useState(false);
+
+    // data
+    const topics = dataTopics.topics;
     const context = useContext(UserContext);
     const comments = context.comments;
+
+    const toggleClass = () => {
+        setIsActive(!isActive);
+    };
 
     const topicSelectHandler = function (event) {
         const selectedTopic = event.target.innerText;
         context.filterComments(selectedTopic);
-        setIsActive(!isActive);
+        toggleClass();
     };
     const parentComments = comments.filter((comment) => comment.parentId === null);
 
@@ -25,30 +34,11 @@ const Main = function () {
             <header className='main__header'>
                 <h1 className='header-huge'>Topics</h1>
                 <div className='topics'>
-                    <div className='topics__btn' onClick={topicSelectHandler}>
-                        Hobbies
-                    </div>
-                    <div className='topics__btn' onClick={topicSelectHandler}>
-                        Travel
-                    </div>
-                    <div className='topics__btn' onClick={topicSelectHandler}>
-                        Health
-                    </div>
-                    <div className='topics__btn' onClick={topicSelectHandler}>
-                        food
-                    </div>
-                    <div className='topics__btn' onClick={topicSelectHandler}>
-                        Tech
-                    </div>
-                    <div className='topics__btn' onClick={topicSelectHandler}>
-                        animals
-                    </div>
-                    <div className='topics__btn' onClick={topicSelectHandler}>
-                        family
-                    </div>
-                    <div className='topics__btn' onClick={topicSelectHandler}>
-                        life
-                    </div>
+                    {topics.map((topic) => (
+                        <div key={topic.id} className='topics__btn' onClick={topicSelectHandler}>
+                            {topic.name}
+                        </div>
+                    ))}
                 </div>
             </header>
             {parentComments.map((comment) => (
