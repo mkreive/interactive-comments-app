2ce7da07925e84bed8a1074fbf61226701a5097e
mkreive <mkreive@gmail.com>
2022-08-10 11:12:58 +0300

    addcomment component added

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -3,15 +3,40 @@ import '../index.scss';
 import UserContext from '../store/user-context';
 
 const AddComment = function () {
+    // states
+
+    // data
     const context = useContext(UserContext);
+    const user = context.user;
+    const today = new Date().toLocaleDateString();
+
+    // handlers
+    const addCommentHandler = function () {};
 
     return (
         <div className='card'>
-            <div className='reply'>
-                <img className='images__photo--reply' src={context.user.image} alt='user photo' />
-                <textarea name='comment' id='123' rows='3' className='text--area'></textarea>
-                <button className='btn btn--blue'>Comment</button>
+            <div className='comment'>
+                <article className='comment__main'>
+                    <header className='comment__main__header'>
+                        <img
+                            className='images__photo'
+                            src={
+                                user.image ||
+                                'https://res.cloudinary.com/kreiva/image/upload/v1660117424/Interactive%20comments%20app/anonymous_iqa63x.png'
+                            }
+                            alt='user-photo'
+                        />
+                        <h3 className='header-small'>{user.username}</h3>
+                        {<span className='header-small-colored'>you</span>}
+                        <span className='header-small-gray'>{today}</span>
+                        <div className='header-reply' onClick={addCommentHandler}>
+                            Post
+                        </div>
+                    </header>
+                </article>
             </div>
+
+            <textarea name='comment' id='123' rows='3' className='text--area '></textarea>
         </div>
     );
 };
